c705e4d39a7668af127c5adabf21e8a69f061c54
samvancart <32706927+samvancart@users.noreply.github.com>
2025-03-13 12:19:58 +0200

    save init vars

--- general/functions.r
+++ general/functions.r
@@ -1214,6 +1214,11 @@ create_prebas_input.f = function(r_no, clim, data.sample, nYears,
   if(!exists("ftTapioParX")) ftTapioParX = ftTapio
   if(!exists("tTapioParX")) tTapioParX = tTapio
   initVar[,6,] <- aaply(initVar,1,findHcNAs,pHcM,pCrobasX,HcModVx)[,6,]*HcFactorX
+
+  ### SAVE INIT VAR
+  save(data.sample, initVar, siteInfo, file = "/scratch/project_2000994/PREBASruns/finRuns/Rsrc/samuel/llindfors/initVars.rdata")
+  ####
+  
   initPrebas <- InitMultiSite(nYearsMS = rep(nYears,nSites),siteInfo=siteInfo,
                               # litterSize = litterSize,#pAWEN = parsAWEN,
                               pCROBAS = pCrobasX,
